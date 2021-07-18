b025b5ad96ca3cb618baac23013af3e07bfde2e8
Kong Yang <kpyang2414@gmail.com>
2021-07-17 21:26:43 -0500

    working on router.js post

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,9 +2,5 @@ const express = require('express');
 const router = express.router();
 const pool = require('../modules/pool');
 
-// This will get all feedback from the feedback
-router.get('/')
-
-
 // This will post to database entire feedback
 router.post('/')
--- src/ReviewFeedback/ReviewFeedback.jsx
+++ src/ReviewFeedback/ReviewFeedback.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import { useHistory } from 'react-router';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 function ReviewFeedback() {
 
+    //importing in all reducer to access their state data
     const feelingRating = useSelector(store => store.feelingReducer);
     const understandingRating = useSelector(store => store.understandingReducer);
     const supportRating = useSelector(store => store.supportReducer);
     const commentsInput = useSelector(store => store.commentsReducer);
     const history = useHistory();
 
-    const handleSubmit = () => {
-      history.push("/form-complete");
+    // posting to database
+    const handleSubmit = (event) => {
+        console.log('Inside ReviewFeedback handleSubmit');
+        event.preventDefault();
+
+      axios({
+          method: "POST",
+          url: "/feedback",
+          data: {
+              feeling: feelingRating,
+              understanding: understandingRating,
+              support: supportRating,
+              comments: commentsInput
+          },
+      })
+      .then( response => {
+          console.log(response);
+          history.push("/form-complete");
+      })
+      .catch(error => {
+          console.log('unable to post feedback,', error);
+          alert('Unable to submit your feedback, please try again.')
+      })
     }
 
     return(
